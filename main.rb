f18715222ec956c06b3981e4ed1fcc482493ffa4
Benji Wiebe <benjiwiebe14@gmail.com>
2019-12-25 19:27:17 -0600

    Put dbpath in YAML config file

--- main.rb
+++ main.rb
@@ -4,14 +4,13 @@ require 'yaml'
 require './lib.rb'
 #TODO if this months entries are < 3, check if this month has any entries in the db yet. if not, then download last months before inserting this months. maybe save this months in a temporary hash
 outfile = "/tmp/fmma.txt"
-dbpath = "db/fmma_nonprod.db"
 cfgfile = 'fmma.config'
 
 cfg = YAML.load(File.open(cfgfile).read)
 
-fmma = FMMA.new(outfile, dbpath)
-fmma.login(cfg.username, Base64.decode64(cfg.password))
-fmma.download(Base64.decode64(cfg.docpassword))
+fmma = FMMA.new(outfile, cfg["dbpath"])
+fmma.login(cfg["username"], Base64.decode64(cfg["password"]))
+fmma.download(Base64.decode64(cfg["docpassword"]))
 data = fmma.process_txt
 fmma.cleanup
 inserted = fmma.insert_data
